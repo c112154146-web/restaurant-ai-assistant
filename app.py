d98990cdee29fa3cfaa122ca1be387b8fe2ec306
c112154146-web <c112154146@nkust.edu.tw>
2026-05-20 21:49:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -424,7 +424,7 @@ def smart_parse_and_execute(text):
                 break
     
     你的任務:
-    1. 判斷動作(action)：
+    1. 判斷動作(action):
        - 'IN' (進貨/補貨/買了/入庫)
        - 'OUT' (出庫/使用/消耗/出餐/賣了)
        - 'WASTE' (報廢/壞掉/過期/爛掉)
